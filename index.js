4e41bb3b68112cd50ccef56dcd85e8da7ed48662
Jie Lan <lanjie3458@gmail.com>
2018-12-24 18:31:47 -0500

    update map and componentDidMount

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-
-
-class Button extends React.Component{
+class Button extends React.Component {
     style = {
         height: 50,
         width: 200,
@@ -8,89 +6,91 @@ class Button extends React.Component{
         backgroundColor: "white",
         fontSize: 30
     }
-    // state={
-    //     text :0
-    // }
 
     constructor(props) {
         super(props)
-        this.state({
-            text: props.select
-        });
+        this.state={text: this.props.select}
+    }
+    componentWillReceiveProps(props) {
+        this.setState({text: props.select})
     }
 
     render() {
-        // var style = { paddingLeft: "20%", }
-        // if (this.state.correct)
-        //     var res1 = "correct: " + this.state.sum
-        // else
-        //     var res2 = "incorrect: " + this.state.sum
+
         return (
-            <button style={this.style}>
+            <button style={this.style} onClick={() => {
+                this.props.click(this.state.text)}}>
                 {this.state.text}
             </button>
         )
     }
 }
 
+Button.defaultProps = {
+    select: "next",
+};
+
 class Game extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { correct: false, hidden: true, l: 0, r: 0, sum: 0, list:0 }
-    this.click = this.handleclick.bind(this)
-    this.new=this.new.bind(this)
-  }
+    style = { paddingLeft: "20%", }
+    constructor(props) {
+        super(props)
+        let list = [-2, -1, 0, 1, 2]
+        this.state = { correct: false, hidden: true, l: 0, r: 0, sum: 0, list: list}
+        this.selection = this.selection.bind(this)
+        this.newGame = this.newGame.bind(this)
 
-  componentDidMount() {
-    var list = [-2, -1, 0, 1, 2]
-    for (let i = 0; i < 5; i++) {
-      let n = Math.round(Math.random() * 4);
-      let m = list[0];
-      list[0] = list[n];
-      list[n] = m;
     }
-    var l = Math.round(Math.random() * 100)
-    var r = Math.round(Math.random() * 100)
-    var sum = l + r
-    this.setState({
-      correct: false, hidden: true,l: l, r: r, sum: sum, list:list
-    });
-  }
-
-  handleclick(props) {
-    if (this.state.sum === props) {
-      this.setState({
-        correct: true,
-      });
+    selection(props) {
+        if (this.state.sum === props) {
+            this.setState({
+                correct: true,
+            });
+        }
+        this.setState({
+            hidden: false
+        });
     }
-    this.setState({
-      hidden: false
-    });
-  }
 
-  new(props){
-    this.componentDidMount();
-  }
 
-  render() {
-    var style = { paddingLeft: "20%", }
-    if (this.state.correct)
-      var res1 = "correct: " + this.state.sum
-    else
-      var res2 = "incorrect: " + this.state.sum
-    return (
-      <div>
-        <h1>What is {this.state.l} + {this.state.r} ?</h1>
-        <div><Button click={this.click} select={89} /></div>
-        <div><Button click={this.click} select={this.state.sum - this.state.list[1]} /></div>
-        <div><Button click={this.click} select={this.state.sum - this.state.list[2]} /></div>
-        <div><Button click={this.click} select={this.state.sum - this.state.list[3]} /></div>
-        <div><Button click={this.click} select={this.state.sum - this.state.list[4]} /></div>
-        <div><Button click={this.new} select={"next"}/></div>
-        <div style={style}>{this.state.hidden ? null : this.state.correct ? res1 : res2}</div>
-      </div>
-    )
-  }
+    componentDidMount(){
+        console.log("componendWillMount")
+        this.newGame()
+    }
+
+    newGame(props) {
+        let list = [-2, -1, 0, 1, 2]
+        for (let i = 0; i < 5; i++) {
+            let n = Math.round(Math.random() * 4);
+            let m = list[0];
+            list[0] = list[n];
+            list[n] = m;
+        }
+        let l = Math.round(Math.random() * 100)
+        let r = Math.round(Math.random() * 100)
+        let sum = l + r
+        this.setState({ correct: false, hidden: true, l: l, r: r, sum: sum, list: list })
+
+    }
+
+    render() {
+        var res;
+        this.state.correct?
+            res = "correct: " + this.state.sum:
+            res = "incorrect: " + this.state.sum
+        return (
+            <div>
+                <h1>What is {this.state.l} + {this.state.r} ?</h1>
+
+                <div>
+                    {this.state.list.map((listNum,index) => (
+                        <div><Button click={this.selection} select={this.state.sum - listNum} key={index} /></div>
+                    ))}
+                </div>
+                <div><Button click={this.newGame} />   </div>
+                <div style={this.style}>{this.state.hidden ? null : this.state.correct ? res : res}</div>
+            </div>
+        )
+    }
 }
 
 const rootElement = document.getElementById("root");
