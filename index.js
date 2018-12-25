c38368e7d16e42a248b1b8fb97aed20ff63d0655
Jie Lan <lanjie3458@gmail.com>
2018-12-24 19:15:05 -0500

    new operator

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ class Game extends React.Component {
     constructor(props) {
         super(props)
         let list = [-2, -1, 0, 1, 2]
-        this.state = { correct: false, hidden: true, l: 0, r: 0, sum: 0, list: list}
+        this.state = { correct: false, hidden: true, l: 0, r: 0, sum: 0, list: list, operator:0}
         this.selection = this.selection.bind(this)
         this.newGame = this.newGame.bind(this)
 
@@ -67,27 +67,56 @@ class Game extends React.Component {
         }
         let l = Math.round(Math.random() * 100)
         let r = Math.round(Math.random() * 100)
-        let sum = l + r
-        this.setState({ correct: false, hidden: true, l: l, r: r, sum: sum, list: list })
+        let sum = 0
+        let operator =  Math.floor(Math.random() * Math.floor(4))
+
+        switch(operator) {
+            case 0:
+                sum = l+r
+                break;
+            case 1:
+                sum = l-r
+                break;
+            case 2:
+                sum = l*r
+                break;
+            default:
+                sum = (l/r).toFixed(2)
+        }
+        console.log("left is"+l,"right is"+r,"sum is"+sum,"operatpor is"+operator)
+
+        this.setState({ correct: false, hidden: true, l: l, r: r, sum: sum, list: list ,operator:operator })
 
     }
 
     render() {
-        var res;
-        this.state.correct?
-            res = "correct: " + this.state.sum:
-            res = "incorrect: " + this.state.sum
+        let operator
+        switch(this.state.operator) {
+            case 0:
+                operator = '+'
+                break;
+            case 1:
+                operator = '-'
+                break;
+            case 2:
+                operator = '*'
+                break;
+            default:
+                operator = '/'
+        }
+
+        let hint=this.state.correct?"correct: " + this.state.sum  :    "incorrect :  " + this.state.sum
         return (
             <div>
-                <h1>What is {this.state.l} + {this.state.r} ?</h1>
+                <h1>What is {this.state.l} {operator} {this.state.r} ?</h1>
 
                 <div>
                     {this.state.list.map((listNum,index) => (
-                        <div><Button click={this.selection} select={this.state.sum - listNum} key={index} /></div>
+                        <div><Button click={this.selection} select={this.state.operator == 3?(this.state.sum - listNum).toFixed(2):(this.state.sum - listNum)} key={index} /></div>
                     ))}
                 </div>
                 <div><Button click={this.newGame} />   </div>
-                <div style={this.style}>{this.state.hidden ? null : this.state.correct ? res : res}</div>
+                <div style={this.style}>{this.state.hidden ? null : hint}</div>
             </div>
         )
     }
